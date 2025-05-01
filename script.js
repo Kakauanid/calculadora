07d60d8723c79c1713da6128ed1ebccb45e1fd45
Kakauanid <kauanpsn15a@gmail.com>
2025-05-01 19:25:55 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function adicionar(valor) {
+    document.getElementById('tela').value +=valor;
+}
+
+function limpar() {
+    document.getElementById('tela').value = '';
+}
+
+function calcular() {
+    try {
+        document.getElementById('tela').value = eval(document.getElementById('tela').value);
+    } catch (e) {
+        alert("Erro na expressão!");
+    }
+}
